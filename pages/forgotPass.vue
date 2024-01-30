c8fe972920a35bcb04be65ee45a95464eb2000c3
sahar <saharhallaji.dev@gmail.com>
2024-01-30 23:31:09 +0330

    forgot password pages UI.

---
+++ pages/forgotPass.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {definePageMeta} from "#imports"
+definePageMeta({ layout: "account" })
+useHead({ title: "Sign in" })
+</script>
+
+<template>
+      <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+        <p class="block text-center text-sm font-medium text-gray-900 dark:text-white">We Sent You a Link to Your Email.</p>
+      </div>
+
+</template>
+
+<style scoped>
+
+</style>
